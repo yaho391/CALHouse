fab3b674cf9b84c5f88ff3aead9da139561e1560
yaho391 <poiuyt290616@gmail.com>
2026-05-28 23:56:29 +0300

    feat: vipe default devices and full working https protocol

--- backend/CalHouse.Api/obj/Debug/net8.0/CalHouse.Api.AssemblyInfo.cs
+++ backend/CalHouse.Api/obj/Debug/net8.0/CalHouse.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalHouse.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13e70abce999e8df7a577a40adea2bd76cfa3731")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0577016e13a559fa739591e5b343683378224f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalHouse.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalHouse.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
